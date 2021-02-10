b1eb7018ad29efa29caa3b4f83b23981e866f634
Cole Contreras <colecontreras@Coles-MacBook-Pro.local>
2021-02-09 21:04:27 -0600

    enabled mac fetch script

--- fetch/macPull.py
+++ fetch/macPull.py
@@ -1,10 +1,13 @@
 import os
-import json
 from typing import Match
 from bs4 import BeautifulSoup
 import requests
+import csv
+from datetime import date
+import pandas as pd
 
-tickers = {'SQQQ', 'NOK', 'IDEX', 'AMD'}
+tickers = ['SQQQ', 'NOK', 'IDEX', 'AMD', 'BTCUSD']
+ticker = list(tickers)
 
 # puts list of tickers into folder as html request
 for name in tickers:
@@ -16,19 +19,24 @@ for name in tickers:
     print(getURL)
     os.system(getURL)
 
-# reading 90% chance between
+
 for name in tickers:
     location = '../tickers/'
     location = location + name + '.html'
-    print(location)
+    print("\n", location)
     with open(location) as html_file:
         soup = BeautifulSoup(html_file, 'lxml')
+
+    # name list
+    exec(name+'=[]')
 
     # rating score
     score = soup.find('div', class_='gauge pull-right exampleDynamicGauge')
     score = score.attrs.get('data-value')
     print(score)
+    exec(name+'.append(score)')
 
+    # 90% chance between first and second
     first = soup.find_all('p', class_="text-justified")[1].find_all('strong')[1].text
     global second
     if first[0] != '$':
@@ -37,3 +45,35 @@ for name in tickers:
     else:
         second = soup.find_all('p', class_="text-justified")[1].find_all('strong')[2].text
     print(first, second)
+    first = str(first)
+    second = str(second)
+    hold = first + " to " + second
+    exec(name+'.append(hold)')
+
+    # opening price
+    try: 
+        opening = soup.find('div', class_="col-lg-5 col-md-12").find('span', class_="text-danger bold").text
+    except Exception as e:
+        opening = soup.find('div', class_="col-lg-5 col-md-12").find('span', class_="text-success bold").text
+    print(opening)
+    exec(name+'.append(opening)')
+
+    # RSI
+    rsi = soup.find('small', class_="mb-0 mt-5").find('strong').text
+    try:
+        rsi = int(rsi)
+    except ValueError:
+        rsi = soup.find('small', class_="mb-0 mt-5").find_all('strong')[1].text
+    print(rsi)
+    exec(name+'.append(rsi)')
+
+
+# dataframe
+# today as the csv name
+today = date.today()
+today = str(today)
+today += "_stocks.csv"
+data = {'SQQQ':SQQQ, 'NOK':NOK, 'IDEX':IDEX, 'AMD':AMD, 'BTCUSD':BTCUSD}
+types = ['Score', '3_Month_Hold', 'Opening_Price', 'RSI']
+df = pd.DataFrame(data, index=types)
+df.to_csv("../" + today)
